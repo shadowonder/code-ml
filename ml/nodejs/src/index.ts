23d78a5c39191fc437fcea712255c718a5ef7d7b
shuowang <shadowonder@gmail.com>
2022-04-23 21:40:33 -0500

    init

---
+++ ml/nodejs/src/index.ts
@@ -0,0 +1,5 @@
+const app = async () => {
+    // Test app
+}
+
+app();
---
+++ ml/python/deep-learning/app/tensorflow/demo.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+a = tf.constant(5.0)
+b = tf.constant(6.0)
+
+print(b)
+
+sum1 = tf.add(a, b)
+print(sum1)
